8bd78c3fd5bfa248f8ce93a5b4199946b25e150f
vagrant <vagrant@homestead>
2018-07-21 10:04:13 +0000

    Add custom helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+require_once _DIR_.'/helpers.php';
 
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
